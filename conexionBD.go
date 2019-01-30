f795996df234424ade00f9595596ab00833eafc3
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-01-30 12:45:43 -0300

    v1.0.1

---
+++ conexionBD.go
@@ -0,0 +1,23 @@
+package conexionBD
+
+import (
+  	"github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
+
+)
+
+var db *gorm.DB
+var err error
+
+
+func connectBD()(*gorm.DB){
+
+	db, err = gorm.Open("postgres", "host=192.168.30.111 port=5432 user=postgres dbname=DES_MULTITENANT_AR_1 password=Post66MM/")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//defer db.Close()
+
+	return db
+}

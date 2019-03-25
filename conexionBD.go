f9d08f8b80757e2edadf8507e6ea3fd0089e9454
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-03-25 12:13:02 -0300

    cambio BD

--- conexionBD.go
+++ conexionBD.go
@@ -10,7 +10,7 @@ var err error
 
 func ConnectBD(tenant string) *gorm.DB {
 
-	db, err = gorm.Open("postgres", "host=192.168.30.111 port=5432 user=postgres dbname=DES2_MULTITENANT_AR_1 password=Post66MM/")
+	db, err = gorm.Open("postgres", "host=192.168.30.111 port=5432 user=postgres dbname=FAF_MULTITENANT_GO password=Post66MM/")
 
 	if err != nil {
 		panic("failed to connect database")

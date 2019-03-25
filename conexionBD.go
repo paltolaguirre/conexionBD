f3357558a151572906bbd3531e095792b5760de9
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-03-25 11:23:44 -0300

    se saco el gorm.Model del struct

--- conexionBD.go
+++ conexionBD.go
@@ -1,23 +1,20 @@
 package conexionBD
 
 import (
-  	"github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
-
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var db *gorm.DB
 var err error
 
+func ConnectBD(tenant string) *gorm.DB {
 
-func ConnectBD(tenant string)(*gorm.DB){
-
-	db, err = gorm.Open("postgres", "host=192.168.30.111 port=5432 user=postgres dbname=DES_MULTITENANT_AR_1 password=Post66MM/")
+	db, err = gorm.Open("postgres", "host=192.168.30.111 port=5432 user=postgres dbname=FAF_MULTITENANT_GO password=Post66MM/")
 
 	if err != nil {
 		panic("failed to connect database")
 	}
 	//defer db.Close()
-
 	return db
 }

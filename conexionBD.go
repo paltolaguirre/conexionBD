1b241983d5c46376ec71e595d428a2b6499a8062
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-04-24 13:02:51 -0300

    prueba para ver si reconoce las foreignkey de public

--- conexionBD.go
+++ conexionBD.go
@@ -26,14 +26,12 @@ func ConnectBD(tenant string) *gorm.DB {
 		db.AutoMigrate(&structLegajo.Pais{}, &structLegajo.Provincia{}, &structLegajo.Localidad{}, &structLegajo.Zona{}, &structLegajo.Modalidadcontratacion{}, &structLegajo.Situacion{}, &structLegajo.Condicion{}, &structLegajo.Condicionsiniestrado{}, &structLegajo.Conveniocolectivo{}, &structLegajo.Centrodecosto{}, &structLegajo.Obrasocial{})
 
 	} else {
-
+		db.Exec("SET search_path = " + tenant + ",public")
 		//para actualizar tablas...agrega columnas e indices, pero no elimina
 		db.AutoMigrate(&structLegajo.Conyuge{}, &structLegajo.Hijo{}, &structLegajo.Legajo{})
 
 		db.Model(&structLegajo.Hijo{}).AddForeignKey("legajoid", "legajo(id)", "CASCADE", "CASCADE")
 		db.Model(&structLegajo.Conyuge{}).AddForeignKey("legajoid", "legajo(id)", "CASCADE", "CASCADE")
-
-		db.Model(&structLegajo.Legajo{}).AddForeignKey("public.localidad.id", "public.localidad(id)", "CASCADE", "CASCADE")
 
 		db.Exec("SET search_path = " + tenant + ",public")
 

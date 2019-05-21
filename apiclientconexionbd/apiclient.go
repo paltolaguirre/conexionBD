f0edc424cf2960c473f3fc6d539b0c6a398aff3e
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-21 07:35:17 -0300

    se agrego la creacion de tablas privadas en obtenerdb

--- apiclientconexionbd/apiclient.go
+++ apiclientconexionbd/apiclient.go
@@ -4,13 +4,30 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/xubiosueldos/autenticacion/publico"
 	"github.com/xubiosueldos/conexionBD"
+	"github.com/xubiosueldos/conexionBD/versiondbmicroservicio"
+	"github.com/xubiosueldos/framework/configuracion"
 )
 
-func ObtenerDB(tokenAutenticacion *publico.Security) *gorm.DB {
+func ObtenerDB(tokenAutenticacion *publico.Security, nombreMicroservicio string, automigrateTablasPrivadas func(*gorm.DB)) *gorm.DB {
 
 	token := *tokenAutenticacion
 	tenant := token.Tenant
 
-	return conexionBD.ConnectBD(tenant)
+	db := conexionBD.ConnectBD(tenant)
+
+	crearTablaVersionMicroServicioYPrivadas(nombreMicroservicio, automigrateTablasPrivadas, db)
+
+	return db
+}
+func crearTablaVersionMicroServicioYPrivadas(nombreMicroservicio string, automigrateTablasPrivadas func(*gorm.DB), db *gorm.DB) {
+
+	configuracion := configuracion.GetInstance()
+
+	versiondbmicroservicio.CrearTablaVersionDBMicroservicio(db)
+
+	if configuracion.Versionlegajo > versiondbmicroservicio.UltimaVersion(nombreMicroservicio, db) {
+		automigrateTablasPrivadas(db)
+		versiondbmicroservicio.ActualizarVersionMicroservicio(db, configuracion.Versionlegajo, nombreMicroservicio)
+	}
 
 }

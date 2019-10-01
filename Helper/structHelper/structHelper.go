6ab66b88f1aa0b58f06245005c9e6481aae100eb
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-10-01 15:19:06 -0300

    [feature/paltolaguirre-svc34]
    Nuevos struct para manejar la informacion de empresa y helper

---
+++ Helper/structHelper/structHelper.go
@@ -0,0 +1,8 @@
+package structHelper
+
+type Helper struct {
+	ID          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Codigo      string `json:"codigo"`
+	Descripcion string `json:"descripcion"`
+}

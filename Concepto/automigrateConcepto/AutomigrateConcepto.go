f98d328ed8f770af1b2f2ab5c63d47725d35cbb5
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-11-14 12:20:19 -0300

    [feature/paltolaguirre-svc3]
    Nuevos atributos en Concepto para el calculo automatico

--- Concepto/automigrateConcepto/AutomigrateConcepto.go
+++ Concepto/automigrateConcepto/AutomigrateConcepto.go
@@ -19,20 +19,14 @@ func AutomigrateConceptoTablasPrivadas(db *gorm.DB) error {
 
 func AutomigrateConceptoTablasPublicas(db *gorm.DB) error {
 	//para actualizar tablas...agrega columnas e indices, pero no elimina
-	err := db.AutoMigrate(&structConcepto.Concepto{}, &structConcepto.Tipoconcepto{}).Error
+	err := db.AutoMigrate(&structConcepto.Concepto{}, &structConcepto.Tipoconcepto{}, &structConcepto.Tipodecalculo{}).Error
 	if err == nil {
-		var tipoConcepto structConcepto.Tipoconcepto
-		db.Raw("SELECT * FROM TIPOCONCEPTO").Scan(&tipoConcepto)
-		if tipoConcepto.ID == 0 {
-			db.Exec("INSERT INTO TIPOCONCEPTO(created_at, nombre, codigo, descripcion, activo) VALUES (current_timestamp,'Importe Remunerativo','IMPORTE_REMUNERATIVO','',1),(current_timestamp,'Importe No Remunerativo','IMPORTE_NO_REMUNERATIVO','',1),(current_timestamp,'Descuento','DESCUENTO','',1),(current_timestamp,'Retención','RETENCION','',1),(current_timestamp,'Aporte Patronal','APORTE_PATRONAL','',1);")
-			db.Exec("UPDATE public.tipoconcepto SET id = -id;")
+		var tipodeCalculo structConcepto.Tipodecalculo
+		db.Raw("SELECT * FROM TIPODECALCULO").Scan(&tipodeCalculo)
+		if tipodeCalculo.ID == 0 {
+			db.Exec("INSERT INTO TIPODECALCULO(created_at, nombre, codigo, descripcion, activo) VALUES (current_timestamp,'Remunerativos','REMUNERATIVOS','',1),(current_timestamp,'No Remunerativos','NO_REMUNERATIVOS','',1),(current_timestamp,'Remunerativos - Descuentos','REMUNERATIVOS_DESCUENTOS','',1),(current_timestamp,'Remunerativos + No Remunerativos','REMUNERATIVOS_NO_REMUNERATIVOS','',1),(current_timestamp,'Remunerativos + No Remunerativos - Descuentos','REMUNERATIVOS_NO_REMUNERATIVOS_DESCUENTOS','',1);")
+			db.Exec("UPDATE public.tipodecalculo SET id = -id;")
 		}
-		db.Exec("UPDATE concepto SET tipoconceptoid = -1 ,esnovedad = false WHERE id IN (-1,-2,-3,-4)")
-		db.Exec("UPDATE concepto SET tipoconceptoid = -1 ,esnovedad = true WHERE id IN (-5,-6)")
-		db.Exec("UPDATE concepto SET tipoconceptoid = -2 ,esnovedad = false WHERE id IN (-7,-8,-9,-10,-11,-12,-13,-14)")
-		db.Exec("UPDATE concepto SET tipoconceptoid = -3 ,esnovedad = true WHERE id IN (-15,-16,-17)")
-		db.Exec("UPDATE concepto SET tipoconceptoid = -4 ,esnovedad = false WHERE id IN (-18,-19,-20)")
-		db.Exec("UPDATE concepto SET tipoconceptoid = -5 ,esnovedad = false WHERE id IN (-21,-22,-23,-24,-25,-26,-27,-28)")
 
 	}
 	return err
--- Concepto/structConcepto/structConcepto.go
+++ Concepto/structConcepto/structConcepto.go
@@ -5,15 +5,18 @@ import "github.com/xubiosueldos/conexionBD/structGormModel"
 //NO ESTOY USANDO EL MODEL DE GORM PORQUE SOLO USA UNSIGNED INTS ENTONCES NO PUEDO USAR ID NEGATIVOS
 type Concepto struct {
 	structGormModel.GormModel
-	Nombre         *string       `json:"nombre" gorm:"not null"`
-	Codigo         string        `json:"codigo"`
-	Descripcion    string        `json:"descripcion"`
-	Activo         int           `json:"activo"`
-	Tipo           string        `json:"tipo"`
-	CuentaContable *int          `json:"cuentacontableid" gorm:"not null"`
-	Cuentacontable *Cuenta       `json:"cuentacontable"`
-	Esimprimible   bool          `json:"esimprimible"`
-	Tipoconcepto   *Tipoconcepto `json:"tipoconcepto" gorm:"ForeignKey:Tipoconceptoid;association_foreignkey:ID;association_autoupdate:false;not null"`
-	Tipoconceptoid *int          `json:"tipoconceptoid" gorm:"not null;default:0"`
-	Esnovedad      bool          `json:"esnovedad" gorm:"default:false"`
+	Nombre          *string        `json:"nombre" gorm:"not null"`
+	Codigo          string         `json:"codigo"`
+	Descripcion     string         `json:"descripcion"`
+	Activo          int            `json:"activo"`
+	Tipo            string         `json:"tipo"`
+	CuentaContable  *int           `json:"cuentacontableid" gorm:"not null"`
+	Cuentacontable  *Cuenta        `json:"cuentacontable"`
+	Esimprimible    bool           `json:"esimprimible"`
+	Tipoconcepto    *Tipoconcepto  `json:"tipoconcepto" gorm:"ForeignKey:Tipoconceptoid;association_foreignkey:ID;association_autoupdate:false;not null"`
+	Tipoconceptoid  *int           `json:"tipoconceptoid" gorm:"not null;default:0"`
+	Esnovedad       bool           `json:"esnovedad" gorm:"default:false"`
+	Porcentaje      *float64       `json:"porcentaje" sql:"type:decimal(19,4);"`
+	Tipodecalculo   *Tipodecalculo `json:"tipodecalculo" gorm:"ForeignKey:Tipodecalculoid;association_foreignkey:ID;association_autoupdate:false"`
+	Tipodecalculoid *int           `json:"tipodecalculoid"`
 }

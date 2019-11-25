0c323f77e2ce42775fd28955ac3cd05a5ebe1d69
Carlos <cflores@xubio.com>
2019-11-25 12:19:02 -0300

    Se agrega Liquidacionitems

--- Liquidacion/structLiquidacion/structLiquidacion.go
+++ Liquidacion/structLiquidacion/structLiquidacion.go
@@ -37,4 +37,5 @@ type Liquidacion struct {
 	Estacontabilizada                    bool                      `json:"estacontabilizada"`
 	Asientomanualtransaccionid           int                       `json:"asientomanualtransaccionid"`
 	Asientomanualnombre                  string                    `json:"asientomanualnombre"`
+	Liquidacionitems                     []Liquidacionitem         `json:"liquidacionitems" gorm:"ForeignKey:Liquidacionid;association_foreignkey:ID"`
 }

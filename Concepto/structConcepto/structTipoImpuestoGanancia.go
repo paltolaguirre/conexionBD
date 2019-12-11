6ce208b5e6e49d28f3b9f2ed0e1fab0c198d33e2
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-12-11 15:18:46 -0300

    [feature/paltolaguirre-svc79]
    Nuevo struct tipoimpuestoganancia, nuevos atributos en el struct de concepto y script en el automigrate para completar la tabla tipoimpuestoganancia y modificar la tabla concepto

---
+++ Concepto/structConcepto/structTipoImpuestoGanancia.go
@@ -0,0 +1,11 @@
+package structConcepto
+
+import "github.com/xubiosueldos/conexionBD/structGormModel"
+
+type Tipoimpuestoganancia struct {
+	structGormModel.GormModel
+	Nombre      string `json:"nombre"`
+	Codigo      string `json:"codigo"`
+	Descripcion string `json:"descripcion"`
+	Activo      int    `json:"activo"`
+}

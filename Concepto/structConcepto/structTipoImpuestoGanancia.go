701ca3d02bc99a7cf4a3845d95a1926d9a6437f2
Paula <paltolaguirre@finnegans.com.ar>
2019-12-16 13:45:45 -0300

    [feature/paltolaguirre-svc79]
    Se agregaron nuevos atributos en el struct tipoimpuestoganancia y se pusieron en el script para que se completen con los valores indicados

--- Concepto/structConcepto/structTipoImpuestoGanancia.go
+++ Concepto/structConcepto/structTipoImpuestoGanancia.go
@@ -4,8 +4,13 @@ import "github.com/xubiosueldos/conexionBD/structGormModel"
 
 type Tipoimpuestoganancia struct {
 	structGormModel.GormModel
-	Nombre      string `json:"nombre"`
-	Codigo      string `json:"codigo"`
-	Descripcion string `json:"descripcion"`
-	Activo      int    `json:"activo"`
+	Nombre                      string `json:"nombre"`
+	Codigo                      string `json:"codigo"`
+	Descripcion                 string `json:"descripcion"`
+	Activo                      int    `json:"activo"`
+	Aplicaimporteremunerativo   bool   `json:"aplicaimporteremunerativo"`
+	Aplicaimportenoremunerativo bool   `json:"aplicaimportenoremunerativo"`
+	Aplicadescuento             bool   `json:"aplicadescuento"`
+	Aplicaretencion             bool   `json:"aplicaretencion"`
+	Aplicaaportepatronal        bool   `json:"aplicaaportepatronal"`
 }

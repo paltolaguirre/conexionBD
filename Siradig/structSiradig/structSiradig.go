e85a0650c295f2cd06d5cba11618004d89a1bb21
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2020-01-09 09:41:09 -0300

    [feature/paltolaguirre-svc83]
    Faltaban las ForeignKey de Siradigid en cada uno de sus atributos

--- Siradig/structSiradig/structSiradig.go
+++ Siradig/structSiradig/structSiradig.go
@@ -12,10 +12,10 @@ type Siradig struct {
 	Legajo                            *structLegajo.Legajo                `json:"legajo" gorm:"ForeignKey:Legajoid;association_foreignkey:ID;association_autoupdate:false"`
 	Legajoid                          *int                                `json:"legajoid" sql:"type:int REFERENCES Legajo(ID)" gorm:"not null"`
 	Periodosiradig                    *time.Time                          `json:"periodosiradig"`
-	Detallecargofamiliarsiradig       []Detallecargofamiliarsiradig       `json:"detallecargofamiliarsiradig"`
-	Importegananciasotroempleosiradig []Importegananciasotroempleosiradig `json:"importegananciasotroempleosiradig"`
-	Deducciondesgravacionsiradig      []Deducciondesgravacionsiradig      `json:"deducciondesgravacionsiradig"`
-	Retencionpercepcionsiradig        []Retencionpercepcionsiradig        `json:"retencionpercepcionsiradig"`
-	Beneficiosiradig                  []Beneficiosiradig                  `json:"beneficiosiradig"`
-	Ajustesiradig                     []Ajustesiradig                     `json:"ajustesiradig"`
+	Detallecargofamiliarsiradig       []Detallecargofamiliarsiradig       `json:"detallecargofamiliarsiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
+	Importegananciasotroempleosiradig []Importegananciasotroempleosiradig `json:"importegananciasotroempleosiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
+	Deducciondesgravacionsiradig      []Deducciondesgravacionsiradig      `json:"deducciondesgravacionsiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
+	Retencionpercepcionsiradig        []Retencionpercepcionsiradig        `json:"retencionpercepcionsiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
+	Beneficiosiradig                  []Beneficiosiradig                  `json:"beneficiosiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
+	Ajustesiradig                     []Ajustesiradig                     `json:"ajustesiradig" gorm:"ForeignKey:Siradigid;association_foreignkey:ID"`
 }

20980ea0b1987db9b2e97a5ebb9a08a41f90f9b0
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2020-01-14 10:09:32 -0300

    [feature/paltolaguirre-svc88]
    Cambio en el nombre de la secuencia creada para la tabla topemaximodescuento

--- Scripts/query_crear_y_llenar_tabla_topemaximodescuento.sql
+++ Scripts/query_crear_y_llenar_tabla_topemaximodescuento.sql
@@ -5,20 +5,20 @@ BEGIN
 
  SELECT relkind
 		   FROM   pg_class
-		   WHERE  relname = 'topmaximodescuento_id_seq'  -- sequence name
+		   WHERE  relname = 'topemaximodescuento_id_seq'  -- sequence name
 		   INTO  _kind;
 		   IF NOT FOUND THEN       -- name is free
-		      CREATE SEQUENCE topmaximodescuento_id_seq
+		      CREATE SEQUENCE topemaximodescuento_id_seq
 				  INCREMENT 1
 				  MINVALUE 1
 				  MAXVALUE 9223372036854775807
 				  START 1
 				  CACHE 1;
-				ALTER TABLE topmaximodescuento_id_seq
+				ALTER TABLE topemaximodescuento_id_seq
 				  OWNER TO postgres;
-				GRANT ALL ON SEQUENCE topmaximodescuento_id_seq TO postgres;
-				GRANT SELECT, USAGE ON SEQUENCE topmaximodescuento_id_seq TO rol_super;
-				GRANT SELECT, USAGE ON SEQUENCE topmaximodescuento_id_seq TO rol_common;
+				GRANT ALL ON SEQUENCE topemaximodescuento_id_seq TO postgres;
+				GRANT SELECT, USAGE ON SEQUENCE topemaximodescuento_id_seq TO rol_super;
+				GRANT SELECT, USAGE ON SEQUENCE topemaximodescuento_id_seq TO rol_common;
 		   ELSIF _kind = 'S' THEN  -- sequence exists
 		      -- nada
 		   ELSE                    -- object name exists for different kind
@@ -26,7 +26,7 @@ BEGIN
 		   END IF;
 
 CREATE TABLE topemaximodescuento(
-   id INTEGER NOT NULL DEFAULT nextval('topmaximodescuento_id_seq'::regclass),
+   id INTEGER NOT NULL DEFAULT nextval('topemaximodescuento_id_seq'::regclass),
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP,
@@ -40,7 +40,7 @@ CREATE TABLE topemaximodescuento(
   topesepelio NUMERIC(19,4) NOT NULL,
   topehipotecarios NUMERIC(19,4) NOT NULL,
   anio INT UNIQUE,
-  CONSTRAINT topmaximodescuento_seq PRIMARY KEY (id)
+  CONSTRAINT topemaximodescuento_seq PRIMARY KEY (id)
 )
 
 WITH (

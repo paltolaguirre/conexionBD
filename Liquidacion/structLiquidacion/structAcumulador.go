4663a7ae9a19000b44f26f506354ea75b6becb03
Walter <wschmidt@finnegans.com.ar>
2020-02-11 09:03:41 -0300

    [feature/wschmidt-svc117] Agrego un acumulador

---
+++ Liquidacion/structLiquidacion/structAcumulador.go
@@ -0,0 +1,14 @@
+package structLiquidacion
+
+import (
+	"github.com/xubiosueldos/conexionBD/structGormModel"
+)
+
+type Acumulador struct {
+	structGormModel.GormModel
+	Nombre            string   `json:"nombre"`
+	Orden             int      `json:"orden"`
+	Importe           *float64 `json:"importe" sql:"type:decimal(19,4);" gorm:"not null"`
+	Tope              *float64 `json:"tope" sql:"type:decimal(19,4);"`
+	Liquidacionitemid int      `json:"liquidacionitemid"`
+}

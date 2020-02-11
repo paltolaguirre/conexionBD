2dfbfea057477d34c1604a62174462d162522a8f
Walter <wschmidt@finnegans.com.ar>
2020-02-11 10:20:06 -0300

    [feature/wschmidt-svc117] Agrego codigo y descripcion

--- Liquidacion/structLiquidacion/structAcumulador.go
+++ Liquidacion/structLiquidacion/structAcumulador.go
@@ -7,6 +7,8 @@ import (
 type Acumulador struct {
 	structGormModel.GormModel
 	Nombre            string   `json:"nombre"`
+	Codigo            string   `json:"codigo"`
+	Descripcion       string   `json:"descripcion"`
 	Orden             int      `json:"orden"`
 	Importe           *float64 `json:"importe" sql:"type:decimal(19,4);" gorm:"not null"`
 	Tope              *float64 `json:"tope" sql:"type:decimal(19,4);"`

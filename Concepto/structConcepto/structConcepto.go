dccbff08d1af531db605231ec059678043c16bb6
Walter <wschmidt@xubio.com>
2020-03-04 11:36:03 -0300

    [feature/wschmidt-svc165] Agrego tipocalculoautomatico y formulas al struct concepto

--- Concepto/structConcepto/structConcepto.go
+++ Concepto/structConcepto/structConcepto.go
@@ -1,27 +1,34 @@
 package structConcepto
 
-import "github.com/xubiosueldos/conexionBD/structGormModel"
+import (
+	"github.com/xubiosueldos/conexionBD/Function/structFunction"
+	"github.com/xubiosueldos/conexionBD/structGormModel"
+)
 
 //NO ESTOY USANDO EL MODEL DE GORM PORQUE SOLO USA UNSIGNED INTS ENTONCES NO PUEDO USAR ID NEGATIVOS
 type Concepto struct {
 	structGormModel.GormModel
-	Nombre                  *string                `json:"nombre" gorm:"not null"`
-	Codigo                  string                 `json:"codigo"`
-	Descripcion             string                 `json:"descripcion"`
-	Activo                  int                    `json:"activo"`
-	Tipo                    string                 `json:"tipo"`
-	CuentaContable          *int                   `json:"cuentacontableid" gorm:"not null"`
-	Cuentacontable          *Cuenta                `json:"cuentacontable"`
-	Esimprimible            bool                   `json:"esimprimible"`
-	Tipoconcepto            *Tipoconcepto          `json:"tipoconcepto" gorm:"ForeignKey:Tipoconceptoid;association_foreignkey:ID;association_autoupdate:false;not null"`
-	Tipoconceptoid          *int                   `json:"tipoconceptoid" gorm:"not null;default:0"`
-	Esnovedad               bool                   `json:"esnovedad" gorm:"default:false"`
-	Porcentaje              *float64               `json:"porcentaje" sql:"type:decimal(19,4);"`
-	Tipodecalculo           *Tipodecalculo         `json:"tipodecalculo" gorm:"ForeignKey:Tipodecalculoid;association_foreignkey:ID;association_autoupdate:false"`
-	Tipodecalculoid         *int                   `json:"tipodecalculoid"`
-	Prorrateo               bool                   `json:"prorrateo"`
-	Basesac                 bool                   `json:"basesac"`
-	Tipoimpuestoganancias   *Tipoimpuestoganancias `json:"tipoimpuestoganancias" gorm:"ForeignKey:Tipoimpuestogananciasid;association_foreignkey:ID;association_autoupdate:false"`
-	Tipoimpuestogananciasid *int                   `json:"tipoimpuestogananciasid"`
-	Eseditable              bool                   `json:"eseditable" gorm:"not null; default:true"`
+	Nombre                  *string                  `json:"nombre" gorm:"not null"`
+	Codigo                  string                   `json:"codigo"`
+	Descripcion             string                   `json:"descripcion"`
+	Activo                  int                      `json:"activo"`
+	Tipo                    string                   `json:"tipo"`
+	CuentaContable          *int                     `json:"cuentacontableid" gorm:"not null"`
+	Cuentacontable          *Cuenta                  `json:"cuentacontable"`
+	Esimprimible            bool                     `json:"esimprimible"`
+	Tipoconcepto            *Tipoconcepto            `json:"tipoconcepto" gorm:"ForeignKey:Tipoconceptoid;association_foreignkey:ID;association_autoupdate:false;not null"`
+	Tipoconceptoid          *int                     `json:"tipoconceptoid" gorm:"not null;default:0"`
+	Esnovedad               bool                     `json:"esnovedad" gorm:"default:false"`
+	Porcentaje              *float64                 `json:"porcentaje" sql:"type:decimal(19,4);"`
+	Tipodecalculo           *Tipodecalculo           `json:"tipodecalculo" gorm:"ForeignKey:Tipodecalculoid;association_foreignkey:ID;association_autoupdate:false"`
+	Tipodecalculoid         *int                     `json:"tipodecalculoid"`
+	Prorrateo               bool                     `json:"prorrateo"`
+	Basesac                 bool                     `json:"basesac"`
+	Tipoimpuestoganancias   *Tipoimpuestoganancias   `json:"tipoimpuestoganancias" gorm:"ForeignKey:Tipoimpuestogananciasid;association_foreignkey:ID;association_autoupdate:false"`
+	Tipoimpuestogananciasid *int                     `json:"tipoimpuestogananciasid"`
+	Eseditable              bool                     `json:"eseditable" gorm:"not null; default:true"`
+	Tipocalculoautomatico   *TipoCalculoAutomatico   `json:"tipocalculoautomatico" gorm:"ForeignKey:Tipocalculoautomaticoid;association_foreignkey:ID;association_autoupdate:false"`
+	Tipocalculoautomaticoid *int                     `json:"tipocalculoautomaticoid"`
+	Formula                 *structFunction.Function `json:"formula" gorm:"ForeignKey:formulaid;association_foreignkey:ID;association_autoupdate:false"`
+	Formulaid               *int                     `json:"formulaid"`
 }

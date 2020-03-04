fb1ef651fb097475e9fb6c08ced19e0d1cf017d1
Paula <paltolaguirre@xubio.com>
2020-03-04 15:18:58 -0300

    [feature/paltolaguirre-svc172]
    Se agrego la referencia que faltaba a concepto

--- Liquidacion/structLiquidacion/structLiquidacionItem.go
+++ Liquidacion/structLiquidacion/structLiquidacionItem.go
@@ -8,7 +8,7 @@ import (
 type Liquidacionitem struct {
 	structGormModel.GormModel
 	Concepto        *structConcepto.Concepto `json:"concepto" gorm:"ForeignKey:Conceptoid;association_foreignkey:ID;association_autoupdate:false;not null"`
-	Conceptoid      *int                     `json:"conceptoid" gorm:"not null"`
+	Conceptoid      *int                     `json:"conceptoid" sql:"type:int REFERENCES Concepto(ID)" gorm:"not null"`
 	Importeunitario *float64                 `json:"importeunitario" sql:"type:decimal(19,4);" gorm:"not null"`
 	Liquidacionid   int                      `json:"liquidacionid"`
 	Cantidad        float64                  `json:"cantidad" sql:"type:decimal(19,4);"`

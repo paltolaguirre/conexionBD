d51bd7fc59cacc4607cd63ac40168f82a401dbf3
Paula <paltolaguirre@xubio.com>
2020-03-06 13:54:58 -0300

    [feature/paltolaguirre-svc172]
    Se eliminan los registros que estan rotos antes de crear la foreignkey de concepto

--- Liquidacion/automigrateLiquidacion/AutomigrateLiquidacion.go
+++ Liquidacion/automigrateLiquidacion/AutomigrateLiquidacion.go
@@ -10,18 +10,18 @@ func AutomigrateLiquidacionTablasPrivadas(db *gorm.DB) error {
 	// para actualizar tablas...agrega columnas e indices, pero no elimina
 	err := db.AutoMigrate(&structLiquidacion.Acumulador{}, &structLiquidacion.Liquidacionitem{}, &structLiquidacion.Liquidacion{}).Error
 	if err == nil {
+
+		versionLiquidacionDB := ObtenerVersionLiquidacionDB(db)
+
+		if versionLiquidacionDB < 7 {
+			err = db.Exec("DELETE FROM liquidacionitem WHERE id IN (SELECT li.id FROM liquidacionitem li LEFT JOIN concepto c ON li.conceptoid = c.id WHERE c.id IS NULL").Error
+		}
 		db.Model(&structLiquidacion.Liquidacionitem{}).AddForeignKey("liquidacionid", "liquidacion(id)", "CASCADE", "CASCADE")
 		db.Model(&structLiquidacion.Liquidacionitem{}).AddForeignKey("conceptoid", "concepto(id)", "RESTRICT", "RESTRICT")
 		db.Model(&structLiquidacion.Acumulador{}).AddForeignKey("liquidacionitemid", "liquidacionitem(id)", "CASCADE", "CASCADE")
 
-		versionLiquidacionDB := ObtenerVersionLiquidacionDB(db)
-
 		if versionLiquidacionDB < 4 {
 			err = unificarDatosEnLaTablaLiquidacionItem(db)
-		}
-
-		if versionLiquidacionDB < 7 {
-			err = db.Exec("DELETE FROM liquidacionitem WHERE id IN (SELECT li.id FROM liquidacionitem li LEFT JOIN concepto c ON li.conceptoid = c.id WHERE c.id IS NULL").Error
 		}
 
 	}

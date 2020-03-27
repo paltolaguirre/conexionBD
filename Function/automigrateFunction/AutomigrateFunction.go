56da5e987279acb4ef4e62c83bd55ffdcfda94de
Carlos <carlos.hector.f@gmail.com>
2020-03-26 23:45:20 -0300

    [feature/cflores-svc130]
    Se agregan funciones "ImpuestoALasGanancias" y "ImpuestoALasGananciasDevolucion"

--- Function/automigrateFunction/AutomigrateFunction.go
+++ Function/automigrateFunction/AutomigrateFunction.go
@@ -37,6 +37,12 @@ func AutomigrateFunctionTablasPublicas(db *gorm.DB) error {
 
 			db.Exec("INSERT INTO value(id, created_at, name, valuenumber, valuestring, valueboolean, valueinvokeid, arginvokeid) VALUES(-3,current_timestamp,'return',0,'',false,null,0)")
 			db.Exec("INSERT INTO function(name, created_at, description, origin, type, scope, result, valueid) VALUES('TotalImporteRemunerativo', current_timestamp, 'Dada una liquidacion obtiene la suma total de importes remunerativos de la misma', 'primitive', 'helper', 'public', 'number', -3)")
+
+			db.Exec("INSERT INTO value(id, created_at, name, valuenumber, valuestring, valueboolean, valueinvokeid, arginvokeid) VALUES(-4,current_timestamp,'return',0,'',false,null,0)")
+			db.Exec("INSERT INTO function(name, created_at, description, origin, type, scope, result, valueid) VALUES('ImpuestoALasGanancias', current_timestamp, '', 'primitive', 'helper', 'public', 'number', -4)")
+
+			db.Exec("INSERT INTO value(id, created_at, name, valuenumber, valuestring, valueboolean, valueinvokeid, arginvokeid) VALUES(-5,current_timestamp,'return',0,'',false,null,0)")
+			db.Exec("INSERT INTO function(name, created_at, description, origin, type, scope, result, valueid) VALUES('ImpuestoALasGananciasDevolucion', current_timestamp, '', 'primitive', 'helper', 'public', 'number', -5)")
 		}
 	}
 	return err

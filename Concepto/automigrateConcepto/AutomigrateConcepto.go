e339b8da198f5e4f02d17fe3c480e2442ae5e42f
Walter <wschmidt@xubio.com>
2020-04-07 14:33:43 -0300

    [feature/wschmidt-svc203] Agrego concepto Medicina prepaga y dias de licencia

--- Concepto/automigrateConcepto/AutomigrateConcepto.go
+++ Concepto/automigrateConcepto/AutomigrateConcepto.go
@@ -101,6 +101,11 @@ func AutomigrateConceptoTablasPublicas(db *gorm.DB) error {
 			db.Exec("update concepto set esremvariable = true where id in (-5, -6)")
 		}
 
+		if versionConceptoDB < 14 {
+			db.Exec("INSERT INTO CONCEPTO(id, created_at, nombre, codigo, descripcion, activo, tipo, cuenta_contable, esimprimible, tipoconceptoid, esnovedad, porcentaje, tipodecalculoid, prorrateo, basesac, tipoimpuestogananciasid, eseditable, tipocalculoautomaticoid, formulanombre, esremvariable) VALUES(-33, current_timestamp,'Medicina Prepaga', 'MEDICINA_PREPAGA',  '', 1, '',-46, false, -2,false, null, null, false, true, -2, true, -1, null, false)")
+			db.Exec("INSERT INTO CONCEPTO(id, created_at, nombre, codigo, descripcion, activo, tipo, cuenta_contable, esimprimible, tipoconceptoid, esnovedad, porcentaje, tipodecalculoid, prorrateo, basesac, tipoimpuestogananciasid, eseditable, tipocalculoautomaticoid, formulanombre, esremvariable) VALUES(-34, current_timestamp,'Dias De Licencia', 'DIAS_DE_LICENCIA',  '', 1, '',-46, true, -3,true, null, null, false, true, -1, true, -1, null, false)")
+		}
+
 	}
 	return err
 }

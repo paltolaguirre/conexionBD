1dfcfb3cdc70a6b20798222f39ef158f480e59a5
Walter <wschmidt@xubio.com>
2020-04-15 10:56:36 -0300

    [feature/wschmidt-svc160] Agrego FK concepto

--- Novedad/automigrateNovedad/AutomigrateNovedad.go
+++ Novedad/automigrateNovedad/AutomigrateNovedad.go
@@ -9,6 +9,7 @@ func AutomigrateNovedadTablasPrivadas(db *gorm.DB) error {
 
 	//para actualizar tablas...agrega columnas e indices, pero no elimina
 	err := db.AutoMigrate(&structNovedad.Novedad{}).Error
+	db.Model(&structNovedad.Novedad{}).AddForeignKey("conceptoid", "concepto(id)", "RESTRICT", "RESTRICT")
 	if ObtenerVersionNovedadDB(db) < 2 {
 		// err = db.Exec("alter table novedad alter column cantidad type numeric(19,4);").Error
 		db.Exec("alter table novedad alter column cantidad type numeric(19,4);")

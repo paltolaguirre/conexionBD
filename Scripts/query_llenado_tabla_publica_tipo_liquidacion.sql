4a652d19513bcedd288090872517d880121eb344
Walter <wschmidt@xubio.com>
2020-06-18 11:55:40 -0300

    [feature/wschmidt-svc196] Muevo todas las querys a actualizacion

--- Scripts/query_llenado_tabla_publica_tipo_liquidacion.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tipoliquidacion(created_at, nombre, codigo, descripcion, activo)
-            values
-(current_timestamp,'Importe Remunerativo','IMPORTE_REMUNERATIVO','',1),
-(current_timestamp,'Importe No Remunerativo','IMPORTE_NO_REMUNERATIVO','',1),
-(current_timestamp,'Descuento','DESCUENTO','',1),
-(current_timestamp,'Retención','RETENCION','',1),
-(current_timestamp,'Aporte Patronal','APORTE_PATRONAL','',1);
-
-UPDATE public.tipoliquidacion SET id = -id;

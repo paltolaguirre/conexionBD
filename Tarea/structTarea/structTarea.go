ab125361f18bfb2b40486551eccafc8cece43040
Walter <wschmidt@xubio.com>
2020-07-27 10:12:05 -0300

    [feature/wschmidt-svc269] Agrego estados y cancelacion

--- Tarea/structTarea/structTarea.go
+++ Tarea/structTarea/structTarea.go
@@ -4,7 +4,8 @@ import "github.com/xubiosueldos/conexionBD/structGormModel"
 
 type Tarea struct {
 	structGormModel.GormModel
-	Handler    string      `json:"handler"`
-	TareaItems []Tareaitem `json:"tareaitems" gorm:"ForeignKey:Tareaid;association_foreignkey:ID"`
-	Estado     int         `json:"estado"`
+	Handler       string       `json:"handler"`
+	TareaItems    []Tareaitem  `json:"tareaitems" gorm:"ForeignKey:Tareaid;association_foreignkey:ID"`
+	Estadotarea   *Estadotarea `json:"estadotarea" gorm:"ForeignKey:estadotareaid;association_foreignkey:ID;association_autoupdate:false;not null;"`
+	Estadotareaid *int         `json:"estadotareaid" sql:"type:int REFERENCES EstadoTarea(ID)"`
 }
--- Tarea/structTarea/structTareaitem.go
+++ Tarea/structTarea/structTareaitem.go
@@ -4,8 +4,9 @@ import "github.com/xubiosueldos/conexionBD/structGormModel"
 
 type Tareaitem struct {
 	structGormModel.GormModel
-	Tareaid int    `json:"tareaid"`
-	Json    string `json:"json" sql:"type:JSON;"`
-	Estado  int    `json:"estado"`
-	Detalle string `json:"detalle"`
+	Tareaid           int              `json:"tareaid"`
+	Json              string           `json:"json" sql:"type:JSON;"`
+	Estadotareaitem   *Estadotareaitem `json:"estadotareaitem" gorm:"ForeignKey:estadotareaitemid;association_foreignkey:ID;association_autoupdate:false;not null;"`
+	Estadotareaitemid *int             `json:"estadotareaitemid" sql:"type:int REFERENCES Estadotareaitem(ID)"`
+	Detalle           string           `json:"detalle"`
 }

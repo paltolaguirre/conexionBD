820b09da047aa37ca3c0680cd012f3e1c8e09f4c
Paula <paltolaguirre@xubio.com>
2020-07-31 17:57:42 -0300

    [feature/paltolaguirre-svc64]
    Cambio el id del struct de zona

--- Liquidacion/structLiquidacion/structZona.go
+++ Liquidacion/structLiquidacion/structZona.go
@@ -1,7 +1,11 @@
 package structLiquidacion
 
+import (
+	"github.com/xubiosueldos/conexionBD/structGormModel"
+)
+
 type Zona struct {
-	ID          int    `json:"ID"`
+	structGormModel.GormModel
 	Nombre      string `json:"nombre"`
 	Codigo      string `json:"codigo"`
 	Descripcion string `json:"descripcion"`
